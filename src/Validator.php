67509dbb30869f61c08fa8e084b768609840e236
jjgrainger <github@jjgrainger.co.uk>
2026-02-06 17:01:06 +0000

    update Validator to validate values for selectors

--- src/Validator.php
+++ src/Validator.php
@@ -45,10 +45,8 @@ class Validator
      * @param array<string, mixed> $config
      * @return self
      */
-    public static function make(
-        array $rules,
-        array $config = [],
-    ): self {
+    public static function make(array $rules, array $config = []): self
+    {
         return Factory::makeValidator(new Configuration($rules, $config));
     }
 
@@ -56,40 +54,69 @@ class Validator
      * Validate input.
      *
      * @param array<string, mixed>|InputContract $input
+     * @param ?ResultContract $result
      * @return ResultContract
      */
     public function validate(array|InputContract $input, ?ResultContract $result = null): ResultContract
     {
-        $input = is_array($input) ? new Input($input) : $input;
-        $result = $result ?? new Result;
+        $input = $this->prepareInput($input);
+        $result = $this->prepareResult($result);
 
-        foreach ($this->specification->attributes() as $attribute) {
-            $value = $input->get($attribute);
+        foreach ($this->specification->attributes() as $selector) {
+            $values = $input->values($selector);
 
-            foreach ($this->specification->rules($attribute) as $rule) {
-                if ($rule instanceof NeedsInput) {
-                    $rule->setInput($input);
-                }
+            foreach ($values as $attribute => $value) {
+                foreach ($this->specification->rules($selector) as $rule) {
+                    if ($rule instanceof NeedsInput) {
+                        $rule->setInput($input);
+                    }
 
-                if ($rule->validate($value)) {
-                    continue;
-                }
+                    if ($rule->validate($value)) {
+                        continue;
+                    }
 
-                if ($rule instanceof SkipsOnFailure) {
-                    break;
-                }
+                    if ($rule instanceof SkipsOnFailure) {
+                        break;
+                    }
 
-                $result->add(
-                    $attribute,
-                    $this->formatter->format($rule->message(), $rule->name(), $attribute, $value)
-                );
+                    $result->add(
+                        $attribute,
+                        $this->formatter->format($rule->message(), $rule->name(), $selector, $value)
+                    );
 
-                if ($rule instanceof StopsOnFailure) {
-                    break;
+                    if ($rule instanceof StopsOnFailure) {
+                        break;
+                    }
                 }
             }
         }
 
         return $result;
+    }
+
+    /**
+     * Prepate Input for validation.
+     *
+     * @param array|InputContract $input
+     * @return InputContract
+     */
+    private function prepareInput(array|InputContract $input): InputContract
+    {
+        $input = $input instanceof InputContract ? $input : new Input($input);
+
+        $input->index($this->specification->attributes());
+
+        return $input;
+    }
+
+    /**
+     * Prepare Result for validation.
+     *
+     * @param ResultContract|null $result
+     * @return ResultContract
+     */
+    private function prepareResult(?ResultContract $result = null): ResultContract
+    {
+        return $result ?? new Result;
     }
 }
