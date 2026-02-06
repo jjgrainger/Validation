85b0d55a3dace3c9e1f49184f328a1be19c0951c
jjgrainger <github@jjgrainger.co.uk>
2026-02-06 17:00:39 +0000

    update Input to use Selector for index and values

--- src/Contracts/InputContract.php
+++ src/Contracts/InputContract.php
@@ -4,5 +4,28 @@ namespace Validation\Contracts;
 
 interface InputContract
 {
+    /**
+     * Get a single value from index data by attribute (not selector).
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
     public function get(string $key, mixed $default = null): mixed;
+
+    /**
+     * Return an array of values for a selector.
+     *
+     * @param string $selector
+     * @return array<string, mixed>
+     */
+    public function values(string $selector): array;
+
+    /**
+     * Build an flat index of resolved selectors and their values.
+     *
+     * @param string[] $selectors
+     * @return void
+     */
+    public function index(array $selectors): void;
 }
